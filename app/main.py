dd22a7f69734146e59ee6a2e7ea38cf364d690c0
Khodakov Valery <teeror93@gmail.com>
2024-04-18 21:27:41 +0400

    Правки роутов

--- app/main.py
+++ app/main.py
@@ -1,8 +1,45 @@
-from fastapi import FastAPI
+from datetime import date
+
+from fastapi import FastAPI, Query, Depends
+from typing import Optional
+
+from pydantic import BaseModel
 
 app = FastAPI()
 
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
+class SHotel(BaseModel):
+    address: str
+    name: str
+    stars: int
+
+
+class HotelsSearchArgs:
+    def __init__(self,
+                 location: str,
+                 date_from: date,
+                 date_to: date,
+                 has_spa: Optional[bool] = None,
+                 stars: Optional[int] = Query(None, ge=1, le=5),
+                 ):
+        self.location = location
+        self.date_from = date_from
+        self.date_to = date_to
+        self.has_spa = has_spa
+        self.stars = stars
+
+
+@app.get("/hotels", response_model=list[SHotel])
+def get_hotels(search_args: HotelsSearchArgs = Depends()):
+    return search_args
+
+
+class SBooking(BaseModel):
+    room_id: int
+    date_from: date
+    date_to: date
+
+
+@app.get("/bookings")
+def add_booking(booking: SBooking):
+    pass
