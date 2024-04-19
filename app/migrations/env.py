29d0d4d6548402fadd236f6adc9f9cbe6c3a6652
Khodakov Valery <teeror93@gmail.com>
2024-04-19 15:42:20 +0400

    Добавлены модели юзеров, комнат и заказов
    Выполнена миграция БД

--- app/migrations/env.py
+++ app/migrations/env.py
@@ -10,7 +10,9 @@ from alembic import context
 sys.path.insert(0, dirname(dirname(dirname(abspath(__file__)))))
 
 from app.database import Base, DATABASE_URL  # noqa
-from app.hotels.models import Hotels  # noqa
+from app.hotels.models import Hotels, Rooms  # noqa
+from app.bookings.models import Bookings  # noqa
+from app.users.models import Users  # noqa
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
