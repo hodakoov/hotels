cfe7fe3e3ac2ebee42d96369a1c82d8458c871a3
Khodakov Valery <teeror93@gmail.com>
2024-04-20 16:04:33 +0400

    Добавлена регистрация пользователя

--- app/main.py
+++ app/main.py
@@ -5,9 +5,11 @@ from typing import Optional
 from pydantic import BaseModel
 
 from app.bookings.router import router as booking_router
+from app.users.router import router as users_router
 
 app = FastAPI()
 
+app.include_router(users_router)
 app.include_router(booking_router)
 
 
