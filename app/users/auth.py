5a58dd631d394ef4ffeaf2a20f4985426f48ef34
Khodakov Valery <teeror93@gmail.com>
2024-04-20 16:42:30 +0400

    Добавлена авторизация пользователя

--- app/users/auth.py
+++ app/users/auth.py
@@ -1,4 +1,10 @@
+from datetime import datetime, timedelta
+from jose import jwt
 from passlib.context import CryptContext
+from pydantic import EmailStr
+
+from app.users.dao import UserDAO
+from app.config import settings
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
@@ -9,3 +15,21 @@ def get_password_hash(password: str) -> str:
 
 def verify_password(plain_password: str, hashed_password: str) -> bool:
     return pwd_context.verify(plain_password, hashed_password)
+
+
+def create_access_token(data: dict) -> str:
+    to_encode = data.copy()
+    expire = datetime.utcnow() + timedelta(minutes=30)
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(
+        to_encode, settings.SECRET_KEY, settings.ALGORITHM
+    )
+    return encoded_jwt
+
+
+async def authenticate_user(email: EmailStr, password: str):
+    user = await UserDAO.find_one_or_none(email=email)
+    if not user and verify_password(password, user.password):
+        return None
+    return user
+
