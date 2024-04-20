3e274dc2591830609f85cd55faa4998e36d9b5ca
Khodakov Valery <teeror93@gmail.com>
2024-04-20 22:19:58 +0400

    Правки ошибок

--- app/users/dependencies.py
+++ app/users/dependencies.py
@@ -18,10 +18,10 @@ async def get_current_user(token: str = Depends(get_token)):
     try:
         payload = jwt.decode(token, settings.SECRET_KEY, settings.ALGORITHM)
     except JWTError:
-        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED)
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail='Could not validate credentials')
     expire: str = payload.get('exp')
     if not expire or int(expire) < datetime.utcnow().timestamp():
-        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED)
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail='Token expired')
     user_id: str = payload.get('sub')
     if not user_id:
         raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED)
