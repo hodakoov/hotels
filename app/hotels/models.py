970123597f1f501c787160e8abaa86498ca70db9
Khodakov Valery <teeror93@gmail.com>
2024-04-22 19:51:56 +0400

    Небольшое изменение в структуре проекта

--- app/hotels/models.py
+++ app/hotels/models.py
@@ -12,16 +12,3 @@ class Hotels(Base):
     services = Column(JSON)
     room_quantity = Column(Integer, nullable=False)
     image_id = Column(Integer)
-
-
-class Rooms(Base):
-    __tablename__ = 'rooms'
-
-    id = Column(Integer, primary_key=True, autoincrement=True)
-    hotel_id = Column(ForeignKey('hotels.id'), nullable=False)
-    name = Column(String, nullable=False)
-    description = Column(String, nullable=True)
-    price = Column(Integer, nullable=False)
-    services = Column(JSON, nullable=True)
-    quantity = Column(Integer, nullable=False)
-    image_id = Column(Integer)
