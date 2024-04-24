f9d129d55b5a153e847025d7a5fcb29ff2ada14d
Khodakov Valery <teeror93@gmail.com>
2024-04-24 14:44:56 +0400

    правки по отелям

--- app/hotels/router.py
+++ app/hotels/router.py
@@ -1,8 +1,30 @@
-from fastapi import APIRouter
+from datetime import date
+from typing import Optional
 
-router = APIRouter(prefix="/hotels")
+from fastapi import APIRouter, HTTPException, status
+
+from app.hotels.dao import HotelDAO
+from app.hotels.schemas import SHotelInfo, SHotel
+
+router = APIRouter(prefix="/hotels", tags=["Отели"])
 
 
-@router.get("")
-def get_hotels():
-    pass
+@router.get("/{location}")
+async def get_hotels_by_location_and_time(
+    location: str,
+    date_from: date,
+    date_to: date,
+) -> list[SHotelInfo]:
+    if date_from > date_to:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Дата заезда не может быть позже даты выезда")
+    if (date_to - date_from).days > 31:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Невозможно забронировать отель сроком более месяца")
+    hotels = await HotelDAO.find_all(location, date_from, date_to)
+    return hotels
+
+
+@router.get("/id/{hotel_id}", include_in_schema=True)
+async def get_hotel_by_id(
+    hotel_id: int,
+) -> Optional[SHotel]:
+    return await HotelDAO.find_one_or_none(id=hotel_id)
---
+++ app/hotels/schemas.py
@@ -0,0 +1,14 @@
+from pydantic import BaseModel
+
+
+class SHotel(BaseModel):
+    id: int
+    name: str
+    location: str
+    services: list[str]
+    rooms_quantity: int
+    image_id: int
+
+
+class SHotelInfo(SHotel):
+    rooms_left: int
