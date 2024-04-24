96600261fadd7a7c09b6e2f499bd1ea3f653240a
Khodakov Valery <teeror93@gmail.com>
2024-04-24 14:57:23 +0400

    правки по комнатам

--- app/hotels/rooms/router.py
+++ app/hotels/rooms/router.py
@@ -1,6 +1,15 @@
+from datetime import date
+
 from app.hotels.router import router
+from app.hotels.rooms.dao import RoomDAO
+from app.hotels.rooms.schemas import SRoomInfo
 
 
-@router.get("/")
-def get_rooms():
-    pass
+@router.get("/{hotel_id}/rooms")
+async def get_rooms_by_time(
+    hotel_id: int,
+    date_from: date,
+    date_to: date,
+) -> list[SRoomInfo]:
+    rooms = await RoomDAO.find_all(hotel_id, date_from, date_to)
+    return rooms
--- app/main.py
+++ app/main.py
@@ -3,6 +3,7 @@ from fastapi import FastAPI
 from app.bookings.router import router as booking_router
 from app.users.router import router as users_router
 from app.hotels.router import router as hotels_router
+from app.hotels.rooms.router import router as rooms_router
 
 
 app = FastAPI()
@@ -10,4 +11,4 @@ app = FastAPI()
 app.include_router(users_router)
 app.include_router(booking_router)
 app.include_router(hotels_router)
-
+app.include_router(rooms_router)
