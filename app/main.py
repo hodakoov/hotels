2977215fb215ea68f1a708a775939e30f6bf1e65
Khodakov Valery <teeror93@gmail.com>
2024-04-24 23:26:28 +0400

    Сохранение картинок

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
 
 from app.bookings.router import router as booking_router
 from app.users.router import router as users_router
@@ -6,9 +7,13 @@ from app.hotels.router import router as hotels_router
 from app.hotels.rooms.router import router as rooms_router
 
 from app.pages.router import router as pages_router
+from app.images.router import router as images_router
 
 
 app = FastAPI()
+
+staticFiles = StaticFiles(directory="app/static")
+app.mount(path="/static", app=staticFiles, name="static")
 
 app.include_router(users_router)
 app.include_router(booking_router)
@@ -16,3 +21,4 @@ app.include_router(hotels_router)
 app.include_router(rooms_router)
 
 app.include_router(pages_router)
+app.include_router(images_router)
