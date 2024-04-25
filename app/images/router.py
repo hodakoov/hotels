417f36359d26bc817c3a84a9a2691c7758f8aa73
Khodakov Valery <teeror93@gmail.com>
2024-04-25 23:00:22 +0400

    Добавлена очередь celery

--- app/images/router.py
+++ app/images/router.py
@@ -2,6 +2,8 @@ import shutil
 
 from fastapi import APIRouter, UploadFile
 
+from app.tasks.tasks import process_pic
+
 router = APIRouter(
     prefix="/images",
     tags=["Загрузка картинок"]
@@ -9,6 +11,9 @@ router = APIRouter(
 
 
 @router.post("/hotels")
-async def add_hotels_images(name: int, file: UploadFile):
-    with open(f'app/static/images/{name}.webp', 'wb+') as file_object:
+async def add_hotel_image(name: int, file: UploadFile):
+    im_path = f"app/static/images/{name}.webp"
+    with open(im_path, "wb+") as file_object:
         shutil.copyfileobj(file.file, file_object)
+    # Отдаем Celery фоновую задачу на обработку картинки
+    process_pic.delay(im_path)
