6a705c583a6c33a4c4b5e7e9777d73dfb33c98ce
Khodakov Valery <teeror93@gmail.com>
2024-04-29 17:50:15 +0400

    Еще тесты для букинга и отелей

--- app/tests/integration_tests/tests_booking/test_api_booking.py
+++ app/tests/integration_tests/tests_booking/test_api_booking.py
@@ -32,3 +32,15 @@ async def test_add_and_get_booking(
 
     response = await authenticated_ac.get("/bookings")
     assert len(response.json()) == booked_rooms
+
+
+async def test_get_and_delete_booking(authenticated_ac: AsyncClient):
+    response = await authenticated_ac.get("/bookings")
+    existing_bookings = [booking["id"] for booking in response.json()]
+    for booking_id in existing_bookings:
+        response = await authenticated_ac.delete(
+            f"/bookings/{booking_id}",
+        )
+
+    response = await authenticated_ac.get("/bookings")
+    assert len(response.json()) == 0
