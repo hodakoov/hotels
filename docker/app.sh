2ea722a43c8cddaf51c7e608e789a6ec7ad6029f
Khodakov Valery <teeror93@gmail.com>
2024-05-15 22:37:31 +0400

    Небольшие правки по docker
    Инфа взята с офф сайта docker

--- docker/app.sh
+++ docker/app.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-# Функция проверки доступности базы данных
-function wait_for_db() {
-  until pg_isready -h db -p 5432 -U "$POSTGRES_USER"; do
-    echo "Waiting for database..."
-    sleep 2
-  done
-}
-
-wait_for_db
+## Функция проверки доступности базы данных
+#function wait_for_db() {
+#  until pg_isready -h db -p 5432 -U "$POSTGRES_USER"; do
+#    echo "Waiting for database..."
+#    sleep 2
+#  done
+#}
+#
+#wait_for_db
 
 alembic upgrade head
 
